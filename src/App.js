a5885e7d71c63d91167be20afb497d887844f954
Hyuk Jin Choi <sc62070@gmail.com>
2023-03-07 00:03:01 +0900

    finished layout

--- src/App.js
+++ src/App.js
@@ -33,9 +33,18 @@ const App = () => {
               </Switch>
             </div>
           </Layout>
-        </div>
+
         <div className='footer'>
-            
+            <Typography.Title level={5} style={{color:'white', textAlign: 'center' }}>
+                Bitify <br/>
+                All rights reservered
+            </Typography.Title>
+            <Space>
+                <Link to="/">Home</Link>
+                <Link to="/exchanges">Exchanges</Link>
+                <Link to="/news">News</Link>
+            </Space>
+          </div>
         </div>
     </div>
   );
